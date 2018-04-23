5b1f04e1f2306482d678936f1ec23c395703d15e
Andrey Kudryavtsev <akudryav@contractor.jcp.com>
2018-04-23 18:35:09 +0300

    WOC 37 results

--- src/woc_37/DynamicLineIntersection.java
+++ src/woc_37/DynamicLineIntersection.java
@@ -1,4 +1,55 @@
 package woc_37;
 
+import java.util.Scanner;
+
+/**
+ * Problem statement:
+ * <p>
+ * https://www.hackerrank.com/contests/w37/challenges/dynamic-line-intersection
+ */
 public class DynamicLineIntersection {
+  private static int size = (int) Math.pow(10, 5);
+  private static int[] values = new int[size + 1];
+  private static int all = 0;
+
+  private static void dynamicLineIntersection(int n) {
+    for (int i = 0; i < n; i++) {
+      String line = scanner.nextLine();
+      String[] val = line.split(" ");
+      if (val.length == 2) {
+        System.out.println(all + values[Integer.parseInt(val[1])]);
+      } else {
+        fillArray(Integer.parseInt(val[1]), Integer.parseInt(val[2]),
+            "+".equals(val[0]) ? 1 : -1);
+      }
+    }
+  }
+
+  private static void fillArray(int k, int b, int toAdd) {
+    if (k == 1) {
+      all += toAdd;
+      return;
+    }
+    int value = b;
+    while (value <= size) {
+      values[value] += toAdd;
+      value += k;
+    }
+    value = b - k;
+    while (value >= 0) {
+      values[value] += toAdd;
+      value -= k;
+    }
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+    dynamicLineIntersection(n);
+
+    scanner.close();
+  }
 }
--- src/woc_37/SuperiorCharacters.java
+++ src/woc_37/SuperiorCharacters.java
@@ -1,4 +1,57 @@
 package woc_37;
 
+import java.util.Scanner;
+
+/**
+ * Problem statement:
+ * <p>
+ * https://www.hackerrank.com/contests/w37/challenges/superior-characters
+ */
 public class SuperiorCharacters {
+
+  private static long maximumSuperiorCharacters(int[] freq) {
+    long actualCount = 0;
+    long length = 0;
+    for (int i : freq) {
+      int currentValue = i;
+      long toAdd = 0;
+      long freeElements = length - actualCount * 2 - 1;
+      if (freeElements > 0) {
+        long min = Math.min(currentValue, freeElements);
+        currentValue -= min;
+        toAdd += min;
+      }
+      if (currentValue > 0) {
+        long maxToAdd = actualCount * 2;
+        long min = Math.min(maxToAdd, currentValue);
+        toAdd += min / 2;
+      }
+      actualCount += toAdd;
+      length += i;
+    }
+
+    return actualCount;
+
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int t = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int tItr = 0; tItr < t; tItr++) {
+      int[] freq = new int[26];
+
+      String[] freqItems = scanner.nextLine().split(" ");
+      scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+      for (int freqItr = 0; freqItr < 26; freqItr++) {
+        int freqItem = Integer.parseInt(freqItems[freqItr]);
+        freq[freqItr] = freqItem;
+      }
+      System.out.println(maximumSuperiorCharacters(freq));
+    }
+    scanner.close();
+  }
 }
